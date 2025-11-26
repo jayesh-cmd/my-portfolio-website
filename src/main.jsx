9d7de57e51448758c7fc56909d7a685cfccb2985
Jayesh Vishwakarma <jayeshvishwakarma@Jay.local>
2025-11-27 00:31:44 +0530

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { inject } from '@vercel/analytics'   // <-- ADD THIS
+inject();                                   // <-- ADD THIS
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
